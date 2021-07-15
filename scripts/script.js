3822b8a0aee1685493b6f0733346d89005304e77
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-07-15 23:04:35 +0500

    fix: minor fixes all over the project

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,22 @@
-"use strict"
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_about');
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
 let likeButtons = document.querySelectorAll('.element__like-button');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+
+    popup.classList.remove('popup_opened');
+}
+
 for (let likeButton of likeButtons) {
   let fillLike = () => {
     likeButton.classList.toggle('element__like-button-active');
@@ -7,49 +24,18 @@ for (let likeButton of likeButtons) {
   likeButton.addEventListener('click',fillLike);
 }
 
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
 let editProfile = () => {
-    popup.classList.add('popup_opened');
+  popup.classList.add('popup_opened');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+
 }
-editButton.addEventListener('click', editProfile);
 
-
-let closeButton = document.querySelector('.popup__close-button');
 let closePopup = () => {
     popup.classList.remove('popup_opened');
 }
-closeButton.addEventListener('click', closePopup);
 
-
-
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__about');
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-    let nameInputValue = nameInput.value;
-    let jobInputValue = jobInput.value;
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileTitle = document.querySelector('.profile__title');
-    let profileSubtitle = document.querySelector('.profile__subtitle');
-
-
-    // Вставьте новые значения с помощью textContent
-    profileTitle.textContent = nameInputValue;
-    profileSubtitle.textContent = jobInputValue;
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', editProfile);
