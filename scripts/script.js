81403992ad0c07e45e424def7041aa629a4f9e6f
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-07-29 08:00:41 +0500

    feat: elements created from array js

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,34 @@ let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
+const elements = document.querySelector('.elements');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 let popupOpen = () => {
   popup.classList.add('popup_opened');
@@ -25,6 +53,17 @@ let editProfile = () => {
   jobInput.value = profileSubtitle.textContent;
 }
 
+initialCards.forEach(function(card) {
+  const elementTemplate = document.querySelector('#element-template').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = element.querySelector('.element__image');
+  const elementTitle = element.querySelector('.element__title');
+  elementImage.src = card.link;
+  elementTitle.textContent = card.name;
+  elements.append(element);
+});
+
 formElement.addEventListener('submit', formSubmitHandler);
 closeButton.addEventListener('click', popupClose);
 editButton.addEventListener('click', editProfile);
+
