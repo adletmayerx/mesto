ff65720db5dcb39005872fc86a762715e2dfc6ae
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-07-29 08:37:10 +0500

    fix: minor fixes in classes and js

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,30 @@
-let formElement = document.querySelector('.popup__form');
+let formElement = document.querySelector('.popup-edit__form');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_about');
 let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button');
+let popupEdit = document.querySelector('.popup-edit');
+let closeEditPopupButton = document.querySelector('.popup-edit__close-button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
-let popupOpen = () => {
-  popup.classList.add('popup_opened');
+let popupEditOpen = () => {
+  popupEdit.classList.add('popup_opened');
 }
-let popupClose = () => {
-  popup.classList.remove('popup_opened');
+let popupEditClose = () => {
+  popupEdit.classList.remove('popup_opened');
 }
 let formSubmitHandler = evt => {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    popupClose();
+    popupEditClose();
 }
 let editProfile = () => {
-  popupOpen();
+  popupEditOpen();
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-closeButton.addEventListener('click', popupClose);
+closeEditPopupButton.addEventListener('click', popupEditClose);
 editButton.addEventListener('click', editProfile);
