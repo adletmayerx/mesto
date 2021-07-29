bf3d542b460d90e71a9101ceddf55bad817340f0
Artur Sadrtdinov <79194435+asadrtdinov@users.noreply.github.com>
2021-07-29 09:06:08 +0500

    feat: restored code lost with merge

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,66 @@
-let formElement = document.querySelector('.popup-edit__form');
-let nameInput = document.querySelector('.popup__input_type_name');
-let jobInput = document.querySelector('.popup__input_type_about');
-let editButton = document.querySelector('.profile__edit-button');
-let popupEdit = document.querySelector('.popup-edit');
-let closeEditPopupButton = document.querySelector('.popup-edit__close-button');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const formElement = document.querySelector('.popup-edit__form');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_about');
+const editButton = document.querySelector('.profile__edit-button');
+const popupEdit = document.querySelector('.popup-edit');
+const closeEditPopupButton = document.querySelector('.popup-edit__close-button');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const elements = document.querySelector('.elements');
 
-let popupEditOpen = () => {
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function(card) {
+  const elementTemplate = document.querySelector('#element-template').content;
+  const element = elementTemplate.querySelector('.element').cloneNode(true);
+  const elementImage = element.querySelector('.element__image');
+  const elementTitle = element.querySelector('.element__title');
+
+  elementTitle.textContent = card.name;
+  elementImage.src = card.link;
+
+  elements.append(element);
+});
+
+
+const popupEditOpen = () => {
   popupEdit.classList.add('popup_opened');
 }
-let popupEditClose = () => {
+const popupEditClose = () => {
   popupEdit.classList.remove('popup_opened');
 }
-let formSubmitHandler = evt => {
+const formSubmitHandler = evt => {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
     popupEditClose();
 }
-let editProfile = () => {
+const editProfile = () => {
   popupEditOpen();
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
@@ -28,3 +69,4 @@ let editProfile = () => {
 formElement.addEventListener('submit', formSubmitHandler);
 closeEditPopupButton.addEventListener('click', popupEditClose);
 editButton.addEventListener('click', editProfile);
+
